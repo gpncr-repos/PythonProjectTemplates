bca20fd4716a54c7d43ff19643b520649c4ba214
vladushka2000 <nazipovvt@yandex.ru>
2024-09-11 12:46:27 +0500

    feat(config): Добавлен конфиг для приложения

---
+++ {{ cookiecutter.service_name }}/src/config/uvicorn_config.py
@@ -0,0 +1,11 @@
+from config import app_config
+
+config = app_config.app_config
+
+uvicorn_config = {
+    "host": config.app_host,
+    "port": config.app_port,
+    "log_level": "info",
+    "reload": True,
+    "interface": "{{ cookiecutter.async_fastapi }}"
+}
