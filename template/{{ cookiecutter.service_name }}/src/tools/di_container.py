81072b4693dd5a13cebde0c926c2a76d9849716f
vladushka2000 <nazipovvt@yandex.ru>
2024-09-14 18:08:17 +0500

    fix(service): Убран метод call из интерфейса сервиса, добавлен di-контейнер для сервиса, исправлено название эндпоинта

--- template/{{ cookiecutter.service_name }}/src/tools/di_container.py
+++ template/{{ cookiecutter.service_name }}/src/tools/di_container.py
@@ -1,6 +1,6 @@
 from dependency_injector import containers, providers
 
-from tools.factories import domain_factory
+from tools.factories import domain_factory, service_factory
 
 
 class DomainContainer(containers.DeclarativeContainer):
@@ -11,3 +11,11 @@ class DomainContainer(containers.DeclarativeContainer):
     geology_params_factory = providers.Factory(domain_factory.GeologyPropertiesFactory)
     well_factory = providers.Factory(domain_factory.WellFactory)
     cluster_factory = providers.Factory(domain_factory.ClusterFactory)
+
+
+class ServiceContainer(containers.DeclarativeContainer):
+    """
+    DI-контейнер с провайдерами объектов сервисов
+    """
+
+    oil_rate_calc_service_factory = providers.Factory(service_factory.OilRateCalcServiceFactory)
