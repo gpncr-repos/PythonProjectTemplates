256939ad97b6cd65474b6784c8701371d79a99a8
AleksandrDenshchikov <sanyakoenig@gmail.com>
2024-10-07 15:13:36 +0200

    fix(di-containers): DI-контейнер разделен на синхронный и асинхронный

---
+++ {{ cookiecutter.service_name }}/src/tools/di_containers/redis_container.py
@@ -0,0 +1,39 @@
+# thirdparty
+from dependency_injector import containers, providers
+
+# project
+from storage.redis import redis_connection
+from repositories import redis_repository
+from uows import redis_uow
+
+
+class RedisSyncContainer(containers.DeclarativeContainer):
+    """
+    DI-контейнер с синхронными провайдерами для работы с кэшем в бд Redis
+    """
+
+    wiring_config = containers.WiringConfiguration(modules=...)
+
+    redis_sync_conn = providers.Factory(redis_connection.RedisConnection)
+    redis_sync_repository = providers.Factory(
+        redis_repository.SyncRedisRepository, redis_sync_conn
+    )
+    redis_sync_uow = providers.Factory(
+        redis_uow.SyncRedisUOW, redis_sync_repository
+    )
+
+
+class RedisAsyncContainer(containers.DeclarativeContainer):
+    """
+    DI-контейнер с асинхронными провайдерами для работы с кэшем в бд Redis
+    """
+
+    wiring_config = containers.WiringConfiguration(modules=...)
+
+    redis_async_conn = providers.Factory(redis_connection.RedisAsyncConnection)
+    redis_async_repository = providers.Factory(
+        redis_repository.AsyncRedisRepository, redis_async_conn
+    )
+    redis_async_uow = providers.Factory(
+        redis_uow.AsyncRedisUOW, redis_async_repository
+    )
