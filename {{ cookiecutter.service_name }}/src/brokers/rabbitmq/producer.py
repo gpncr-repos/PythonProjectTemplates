ecd208a56a471f8a1b23ed5a2ac1eee704b0c758
vladushka2000 <nazipovvt@yandex.ru>
2024-10-15 18:18:00 +0500

    chore(rabbitmq): Рефакторинг для более удобного взаимодействия с брокером

--- {{ cookiecutter.service_name }}/src/brokers/rabbitmq/producer.py
+++ {{ cookiecutter.service_name }}/src/brokers/rabbitmq/producer.py
@@ -1,6 +1,7 @@
+from __future__ import annotations  # no qa
+
 import aio_pika
 
-from brokers.rabbitmq import routing_configurator
 from config import rabbitmq_config
 from interfaces import base_message_broker, base_proxy, base_rabbitmq_routing_configurator as base_configurator
 from models.dto import broker_message_dto
@@ -8,60 +9,67 @@ from models.dto import broker_message_dto
 config = rabbitmq_config.config
 
 
-class RabbitMQProducer(base_message_broker.BaseProducer, routing_configurator.RoutingConfigurator):
+class RabbitMQProducer(base_message_broker.BaseProducer):
     def __init__(
         self,
         connection_proxy: base_proxy.ConnectionProxy,
-        builder: base_configurator.BaseRoutingBuilder,
+        router: base_configurator.BaseRoutingConfigurator,
         model_type: type[broker_message_dto.BrokerMessageDTO] = broker_message_dto.BrokerMessageDTO
     ) -> None:
         """
         Инициализировать переменные
         :param connection_proxy: прокси-объект соединения
-        :param builder: объект Builder для конфигурации маршрутизации
+        :param router: конфигуратор маршрутизации сообщений
         :param model_type: тип сообщения
         """
 
         self._connection_proxy = connection_proxy
         self._connection: aio_pika.abc.AbstractRobustConnection | None = None
-        self._channel: aio_pika.RobustChannel | None = None
+        self._routing_configurator = router
         self._model_type = model_type
+        self._channel: aio_pika.abc.AbstractRobustChannel | None = None
 
-        super().__init__(builder)
-
-    async def produce(self, exchange: str, routing_key: str, message: broker_message_dto.BrokerMessageDTO) -> None:
+    async def __aenter__(self) -> RabbitMQProducer:
         """
-        Отправить сообщение в обменник
-        :param exchange: название обменника
-        :param routing_key: ключ маршрутизации
-        :param message: сообщение
-        """
-
-        if self._channel is None:
-            raise ValueError("Объект канала не инициализирован")
-
-        if not isinstance(message, self._model_type):
-            raise ValueError("Несоответствие типа сообщения; сообщение не отправлено")
-
-        message = aio_pika.Message(message.model_dump_json(by_alias=True).encode("utf-8"))
-
-        await self._exchanges[config.exchange].publish(message, routing_key)
-
-    async def start(self) -> None:
-        """
-        Запустить продюсер
+        Войти в контекстный менеджер
+        :return: объект продюсера
         """
 
         self._connection = await self._connection_proxy.get_connection()
         self._channel = await self._connection.channel()
-        await self.configure_routes()
 
-    async def stop(self) -> None:
-        """
-        Остановить продюсер
-        """
+        return self
 
-        if self._channel is None:
-            raise ValueError("Объект канала не инициализирован")
+    async def __aexit__(self, *args, **kwargs) -> None:
+        """
+        Выйти из контекстного менеджера
+        """
 
         await self._channel.close()
+        await self._connection_proxy.close_connection()
+
+    async def produce(
+        self,
+        exchange: str,
+        routing_key: str,
+        messages: list[broker_message_dto.BrokerMessageDTO]
+    ) -> None:
+        """
+        Отправить сообщение в обменник
+        :param exchange: название обменника
+        :param routing_key: ключ маршрутизации
+        :param messages: список сообщений
+        """
+
+        if self._connection is None:
+            raise ValueError("Объект соединения не инициализирован")
+
+        await self._routing_configurator.configure_routes(self._channel)
+
+        for message in messages:
+            if not isinstance(message, self._model_type):
+                raise ValueError("Несоответствие типа сообщения; сообщение не отправлено")
+
+            message = aio_pika.Message(message.model_dump_json(by_alias=True).encode("utf-8"))
+
+            await self._routing_configurator.exchanges[config.exchange].publish(message, routing_key)
--- {{ cookiecutter.service_name }}/src/brokers/rabbitmq/routing_configurator.py
+++ {{ cookiecutter.service_name }}/src/brokers/rabbitmq/routing_configurator.py
@@ -14,34 +14,35 @@ class RoutingBuilder(base_configurator.BaseRoutingBuilder):
     pass
 
 
-class RoutingConfigurator(base_configurator.BaseRoutingConfiguratorMixin):
+class RoutingConfigurator(base_configurator.BaseRoutingConfigurator):
     """
     Класс-миксин, конфигурирующий обменники и очереди
     """
 
     _is_declared: bool = False
 
-    async def configure_routes(self) -> None:
+    @classmethod
+    def _set_is_declared_to_true(cls) -> None:
+        """
+        Поставить значение переменной is_declared на True
+        """
+        cls._is_declared = True
+
+    async def configure_routes(self, channel: aio_pika.abc.AbstractRobustChannel, *args, **kwargs) -> None:
         """
         Сконфигурировать обменники и очереди, которые к ним привязаны
+        :param channel: объект канала
         """
 
         if self._is_declared is True:
             return
 
-        exchange = await self._builder.declare_exchange(
-            self._channel,
-            config.exchange,
-            aio_pika.ExchangeType.DIRECT
-        )
-        self._exchanges[exchange.name] = exchange
+        exchange = await self._builder.declare_exchange(channel, config.exchange, aio_pika.ExchangeType.DIRECT)
+        self.exchanges[exchange.name] = exchange
 
-        queue = await self._builder.declare_queue(
-            self._channel,
-            config.queue
-        )
-        self._queues[queue.name] = queue
+        queue = await self._builder.declare_queue(channel, config.queue)
+        self.queues[queue.name] = queue
 
         await self._builder.bind_queue_to_exchange(queue, exchange, config.routing_key)
 
-        self._is_declared = True
+        self._set_is_declared_to_true()
