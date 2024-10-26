128b5da23408f2e68849f0d12501d6a83cf21e2b
vladushka2000 <nazipovvt@yandex.ru>
2024-10-26 14:22:10 +0500

    chore(producer): Изменен продюсер в соответствии с новыми интерфейсами

--- {{ cookiecutter.service_name }}/src/brokers/rabbitmq/producer.py
+++ {{ cookiecutter.service_name }}/src/brokers/rabbitmq/producer.py
@@ -1,9 +1,7 @@
-from __future__ import annotations  # no qa
-
 import aio_pika
 
 from config import rabbitmq_config
-from interfaces import base_message_broker, base_proxy, base_rabbitmq_routing_configurator as base_configurator
+from interfaces import base_message_broker, base_proxy
 from models.dto import broker_message_dto
 
 config = rabbitmq_config.config
@@ -13,69 +11,47 @@ class RabbitMQProducer(base_message_broker.BaseProducer):
     def __init__(
         self,
         connection_proxy: base_proxy.ConnectionProxy,
-        router: base_configurator.BaseRoutingConfigurator,
         model_type: type[broker_message_dto.BrokerMessageDTO] = broker_message_dto.BrokerMessageDTO
     ) -> None:
         """
         Инициализировать переменные
         :param connection_proxy: прокси-объект соединения
-        :param router: конфигуратор маршрутизации сообщений
         :param model_type: тип сообщения
         """
 
         self._connection_proxy = connection_proxy
-        self._connection: aio_pika.abc.AbstractRobustConnection | None = None
-        self._routing_configurator = router
         self._model_type = model_type
+        self._connection: aio_pika.abc.AbstractRobustConnection | None = None
         self._channel: aio_pika.abc.AbstractRobustChannel | None = None
-
-    async def __aenter__(self) -> RabbitMQProducer:
-        """
-        Войти в контекстный менеджер
-        :return: объект продюсера
-        """
-
-        self._connection = await self._connection_proxy.get_connection()
-        self._channel = await self._connection.channel()
-
-        return self
-
-    async def __aexit__(self, *args, **kwargs) -> None:
-        """
-        Выйти из контекстного менеджера
-        """
-
-        await self._channel.close()
-
-    async def stop(self) -> None:
-        """
-        Остановить продюсер
-        """
-
-        await self._connection_proxy.close_connection()
 
     async def produce(
         self,
-        exchange: str,
+        exchange_name: str,
         routing_key: str,
-        messages: list[broker_message_dto.BrokerMessageDTO]
+        message: broker_message_dto.BrokerMessageDTO
     ) -> None:
         """
         Отправить сообщение в обменник
-        :param exchange: название обменника
+        :param exchange_name: название обменника
         :param routing_key: ключ маршрутизации
-        :param messages: список сообщений
+        :param message: сообщение
         """
 
-        if self._connection is None:
-            raise ValueError("Объект соединения не инициализирован")
+        if not isinstance(message, self._model_type):
+            raise ValueError("Несоответствие типа сообщения; сообщение не отправлено")
 
-        await self._routing_configurator.configure_routes(self._channel)
+        self._connection = await self._connection_proxy.connect()
+        self._channel = await self._connection.channel()
 
-        for message in messages:
-            if not isinstance(message, self._model_type):
-                raise ValueError("Несоответствие типа сообщения; сообщение не отправлено")
+        message = aio_pika.Message(message.model_dump_json(by_alias=True).encode("utf-8"))
+        exchange = await self._channel.get_exchange(exchange_name)
 
-            message = aio_pika.Message(message.model_dump_json(by_alias=True).encode("utf-8"))
+        await exchange.publish(message, routing_key)
 
-            await self._routing_configurator.exchanges[config.exchange].publish(message, routing_key)
+    async def disconnect(self) -> any:
+        """
+        Разорвать соединение с брокером
+        """
+
+        await self._channel.close()
+        await self._connection_proxy.disconnect()
