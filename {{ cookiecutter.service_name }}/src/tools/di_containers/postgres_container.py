bc4c4f6b209628f37458dfabc485063774a87efc
vladushka2000 <nazipovvt@yandex.ru>
2024-11-12 22:53:34 +0500

    chore(postgres): Обновлена реализация работы с БД

--- {{ cookiecutter.service_name }}/src/tools/di_containers/postgres_container.py
+++ {{ cookiecutter.service_name }}/src/tools/di_containers/postgres_container.py
@@ -1,11 +1,16 @@
 # thirdparty
+import psycopg_pool
+
 # project
-from config.pg_config import pg_config
+from config import pg_config
 from dependency_injector import containers, providers
-from repositories import psycopg_repository
-from repositories.sql_tools import PsycopgRawSQLCreator
+from interfaces import base_postgres_cursor_proxy as cursor_proxy
+from repositories import raw_postgres_repository
+from repositories.tools import sql_tools
 from storage.psycopg import psycopg_proxy
 from uows import psycopg_uow
+
+config = pg_config.pg_config
 
 
 class PsycopgSyncContainer(containers.DeclarativeContainer):
@@ -14,27 +19,18 @@ class PsycopgSyncContainer(containers.DeclarativeContainer):
     """
 
     # указать связанные модули
-    wiring_config = containers.WiringConfiguration(packages=["web.entrypoints"])
+    wiring_config = containers.WiringConfiguration(modules=None)
 
-    conn_proxy = providers.Factory(psycopg_proxy.PsycopgConnectionProxy, pg_config)
-    sql_creator = providers.Factory(PsycopgRawSQLCreator)
+    dsn = providers.Factory(str, pg_config.pg_config.psycopg_dsn)
+    connection_pool = providers.Singleton(
+        psycopg_pool.ConnectionPool, dsn, max_size=config.connection_pool_size
+    )
+    cursor_type = providers.AbstractFactory(cursor_proxy.BasePsycopgCursorProxy)
+    connection_proxy = providers.Factory(
+        psycopg_proxy.PsycopgConnectionProxy, connection_pool, cursor_type
+    )
+    sql_creator = providers.Factory(sql_tools.PsycopgRawSQLCreator)
     psycopg_repository = providers.Factory(
-        psycopg_repository.PsycopgSyncRepository, conn_proxy, sql_creator
+        raw_postgres_repository.PsycopgSyncRepository, connection_proxy, sql_creator
     )
     psycopg_uow = providers.Factory(psycopg_uow.PsycopgSyncUOW, psycopg_repository)
-
-
-class PsycopgAsyncContainer(containers.DeclarativeContainer):
-    """
-    DI-контейнер с провайдерами для асинхронной работы с БД Postgres через psycopg
-    """
-
-    # указать связанные модули
-    wiring_config = containers.WiringConfiguration(packages=["web.entrypoints"])
-
-    conn_proxy = providers.Factory(psycopg_proxy.PsycopgAsyncConnectionProxy, pg_config)
-    sql_creator = providers.Factory(PsycopgRawSQLCreator)
-    psycopg_repository = providers.Factory(
-        psycopg_repository.PsycopgAsyncRepository, conn_proxy, sql_creator
-    )
-    psycopg_uow = providers.Factory(psycopg_uow.PsycopgAsyncUOW, psycopg_repository)
